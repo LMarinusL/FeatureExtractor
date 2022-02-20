947c33d4507183dac6ae87b1ae50b5b2ecaad182
LMarinusL <lars.m.langhorst@gmail.com>
2022-02-20 13:03:03 +0100

    water height displayed

--- ToggleWater.cs
+++ ToggleWater.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
 
 public class ToggleWater : MonoBehaviour
 {
+    public float height;
+    [SerializeField] public TextMeshProUGUI _valueText;
     [SerializeField] public GameObject water;
     // Start is called before the first frame update
     void Start()
@@ -12,6 +18,8 @@ public class ToggleWater : MonoBehaviour
     }
     void Update()
     {
+        height = Mathf.Round(transform.position.y);
+        _valueText.text = height.ToString();
         if (Input.GetKey(KeyCode.B))
         {
             transform.position += new Vector3(0, 1, 0); ;
