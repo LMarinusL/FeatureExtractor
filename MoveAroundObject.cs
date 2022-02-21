f93aa88bf806adfbd3d970a9a9899638cd845086
LMarinusL <lars.m.langhorst@gmail.com>
2022-02-21 11:53:04 +0100

    camera fixed

--- MoveAroundObject.cs
+++ MoveAroundObject.cs
@@ -18,13 +18,13 @@ public class MoveAroundObject : MonoBehaviour
     private Transform _target;
 
     [SerializeField]
-    private float _distanceFromTarget = 150f;
+    private float _distanceFromTarget = 350f;
 
     void Update()
     {
-        float xCorrection= 1140f;
+        float xCorrection= 0f;
         float yCorrection= 110f;
-        float zCorrection= 644f;
+        float zCorrection= 0f;
         float mouseX = 0f;
         float mouseY = 0f;
 
@@ -39,8 +39,8 @@ public class MoveAroundObject : MonoBehaviour
             _rotationY += mouseX;
         }
         Vector3 newPosition = _target.position + new Vector3(xCorrection, yCorrection, zCorrection );
-        _rotationX = Mathf.Clamp(_rotationX, -60, 60);
-        transform.localEulerAngles = new Vector3(_rotationX, _rotationY, 0);
+        _rotationX = Mathf.Clamp(_rotationX, -150, 150);
+        transform.localEulerAngles = new Vector3(_rotationX +90, _rotationY, 0);
         transform.position = newPosition - transform.forward * _distanceFromTarget;
     }
 }
