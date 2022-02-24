c0a78a1230a022819d9847dcf866689654d8c100
LMarinusL <lars.m.langhorst@gmail.com>
2022-02-24 12:05:33 +0100

    plots added

--- Python/CSConnector.py
+++ Python/CSConnector.py
@@ -51,10 +51,6 @@ import pathlib
 print(pathlib.Path(__file__).parent.resolve())
 df = pd.read_csv('C:/Users/neder/Documents/Geomatics/Unity/PCproject/DEMViewer/Assets/Output/outputGridFull.txt', sep=" ")
 print(df.head())
-
-
-
-
 ##############
 
 from sklearn.model_selection import train_test_split
@@ -94,34 +90,29 @@ features=df.columns[[0,1,2,3,4,6,7,8,9]]
 
 importances = forest2.feature_importances_
 indices = np.argsort(importances)
-
+fig1 = plt.figure()
 plt.figure(1)
 plt.title('Feature Importances')
 plt.barh(range(len(indices)), importances[indices], color='b', align='center')
 plt.yticks(range(len(indices)), features[indices])
 plt.xlabel('Relative Importance')
+plt.show()
 ##################
 ###################
 plt.rcParams.update({'font.size': 20})
-fig, ax = plt.subplots(nrows=1, ncols=3, sharex=True, sharey=True,
+fig2, ax = plt.subplots(nrows=1, ncols=3, sharex=True, sharey=True,
                                     figsize=(20, 10))
-
 cm = plt.cm.get_cmap('RdYlBu')
-
 sc = ax[0].scatter(Xt['x'], Xt['y'],
            linewidths=1, alpha=.7,
             edgecolor='none',
            s = 20,
            c=(yt),
             cmap=cm, vmin=-2, vmax=2)
-
-
 ax[0].set_title('Actual')
 ax[0].set_xlabel("x coordinate")
 ax[0].set_ylabel("y coordinate")
 ax[0].tick_params(labelsize=12)
-
-
 cm = plt.cm.get_cmap('RdYlBu')
 sc = ax[1].scatter(Xt['x'], Xt['y'],
            linewidths=1, alpha=.7,
@@ -129,16 +120,12 @@ sc = ax[1].scatter(Xt['x'], Xt['y'],
            s = 20,
            c=(y_pred),
             cmap=cm, vmin=-2, vmax=2)
-cbar = fig.colorbar(sc)
-
+cbar = fig2.colorbar(sc)
 cbar.ax.set_ylabel('Change in bed level height per year [m]', rotation=270)
 cbar.ax.get_yaxis().labelpad = 20
 ax[1].set_title('Prediction')
 ax[1].set_xlabel("x coordinate")
 ax[1].tick_params(labelsize=12)
-
-
-
 sc = ax[2].scatter(Xt['x'], Xt['y'],
            linewidths=1, alpha=.7,
             edgecolor='none',
@@ -149,10 +136,31 @@ ax[2].set_title('Residual')
 ax[2].set_xlabel("x coordinate")
 ax[2].tick_params(labelsize=12)
 
-fig.subplots_adjust(wspace=0.03, hspace=0)
-
-
-fig.suptitle('Annual sedimentation 2012-2018')
+fig2.subplots_adjust(wspace=0.03, hspace=0)
+fig2.suptitle('Annual sedimentation 2012-2018')
 plt.show()
 
 #######
+print(" shown map residuals")
+fig3 = plt.figure()
+plt.title('2012-2018 Sedimentation')
+plt.boxplot([y_pred, yt, (y_pred - yt)], showfliers=False)
+plt.xticks([1, 2, 3], ['Prediction', 'Actual', 'Residuals'])
+plt.ylabel('[m]')
+plt.show()
+
+###############
+
+fig4 = plt.figure()
+plt.figure(figsize=(12,8))
+plt.title('Random Forest Regressor')
+plt.scatter(y_train_pred, y_train_pred - y_traino, c='blue', marker='o',s=10, label='Training dataset')
+plt.scatter(y_test_pred, y_test_pred - y_testo, c='orange', marker='*',s=10, label='Test dataset')
+plt.scatter(y_pred, y_pred - yt, c='red', marker='.',s=7, label='Future prediction')
+plt.xlabel('Predicted values [m]')
+plt.ylabel('Residuals [m]')
+plt.legend(loc='upper left')
+plt.hlines(y=0, xmin=-50, xmax=100, lw=2, color='k')
+plt.xlim([-1, 3])
+plt.ylim([-3, 2])
+plt.show()
