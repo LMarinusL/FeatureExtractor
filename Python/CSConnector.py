b9186ac19f20fd806a8dfdce3161d423502b00be
LMarinusL <lars.m.langhorst@gmail.com>
2022-03-06 21:29:53 +0100

    added SVR and GPR

--- Python/CSConnector.py
+++ Python/CSConnector.py
@@ -18,6 +18,13 @@ from sklearn.linear_model import Ridge
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPRegressor
 from sklearn import preprocessing
+from sklearn.svm import SVR
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel
+
+import numpy as np
 plt.rcParams.update({'font.size': 16})
 
 #print(pd.__version__)  
@@ -73,9 +80,7 @@ yt = dfTest[param_study]
 
 
 ###################
-
-
-
+# BOXPLOT OF DATA
 #######################
 
 df18 = sklearn.utils.resample(df[df.year == 2018], n_samples=10000, random_state=None, stratify=None)
@@ -90,7 +95,7 @@ plt.xticks([1, 2, 3], ['97-08', '08-12', '12-18'])
 plt.show()
 
 ###################
-
+# TRAINING ALGORITHMS
 ######################
 
 col_study2 = ['interval',  'hprevious', 'hrelative1','hrelative2','hrelative3', 'slope', 'aspect', 'curvature', 'dist', 'averageRunoff1', 'averageRunoff2', 'averageRunoff3', 'discharge','skeletonAngle', 'riverLength', 'inflow']
@@ -103,26 +108,47 @@ Xt2 = dfTest[col_study2]
 yt = dfTest[param_study]
 X_traino2, X_testo2, y_traino2, y_testo2 = train_test_split(Xo2, yo2, test_size=0.3, random_state=42)
 print('split')
-forest3 = RandomForestRegressor()
+forestImportance = RandomForestRegressor()
+forestImportance.fit(X_traino2, y_traino2)
+
+forest3 = make_pipeline(StandardScaler(), RandomForestRegressor())
+
 forest3.fit(X_traino2, y_traino2)
 print('fitted')
+print('rf-predicting: ')
 y_train_pred = forest3.predict(X_traino2)
 y_test_pred = forest3.predict(X_testo2)
 y_pred = forest3.predict(Xt2)
 print('predicted')
 
+print('SVR-predicting: ')
+regr = make_pipeline(StandardScaler(), SVR(C=1.0, epsilon=0.2))
+regr.fit(X_traino2, y_traino2)
+y_predSVR = regr.predict(Xt2)
+print('predicted')
 
+print('Gaussian-predicting: ')
+kernel = DotProduct() + WhiteKernel()
+GPR = make_pipeline(StandardScaler(), GaussianProcessRegressor(kernel=kernel,
+        random_state=0))
+GPR.fit(X_traino2, y_traino2)
+y_predGPR = GPR.predict(Xt2)
+print('predicted')
+
+
+###################################
+# RF IMPORTANCES
 ###############################
 plt.rcParams.update({'font.size': 15})
 
-result = pd.DataFrame(forest3.feature_importances_,  df[col_study2].columns)
+result = pd.DataFrame(forestImportance.feature_importances_,  df[col_study2].columns)
 result.columns = ['importance']
 result.sort_values(by='importance', ascending=False)
 
 print(df.head())
 features=df.columns[[1,4,6,7,8,9, 10, 11, 12, 13, 14, 15, 16,17, 18, 19]]
 
-importances2 = forest3.feature_importances_
+importances2 = forestImportance.feature_importances_
 indices = np.argsort(importances2)
 fig1 = plt.figure()
 plt.title('Feature Importances')
@@ -131,7 +157,8 @@ plt.yticks(range(len(indices)), features[indices])
 plt.xlabel('Relative Importance')
 plt.show()
 ###################################
-
+# RF PLOT
+###################################
 plt.rcParams.update({'font.size': 20})
 fig5, ax = plt.subplots(nrows=1, ncols=3, sharex=True, sharey=True,
                                     figsize=(20, 10))
@@ -169,5 +196,93 @@ ax[2].set_xlabel("x coordinate")
 ax[2].tick_params(labelsize=12)
 
 fig5.subplots_adjust(wspace=0.03, hspace=0)
-fig5.suptitle('Annual sedimentation 2012-2018')
+fig5.suptitle('RF Annual sedimentation 2012-2018')
+plt.show()
+
+###################################
+# SVR PLOT
+###################################
+
+plt.rcParams.update({'font.size': 20})
+fig5, ax = plt.subplots(nrows=1, ncols=3, sharex=True, sharey=True,
+                                    figsize=(20, 10))
+cm = plt.cm.get_cmap('RdYlBu')
+sc = ax[0].scatter(Xt['x'], Xt['y'],
+           linewidths=1, alpha=.7,
+            edgecolor='none',
+           s = 20,
+           c=(yt),
+            cmap=cm, vmin=-2, vmax=2)
+ax[0].set_title('Actual')
+ax[0].set_xlabel("x coordinate")
+ax[0].set_ylabel("y coordinate")
+ax[0].tick_params(labelsize=12)
+sc = ax[1].scatter(Xt['x'], Xt['y'],
+           linewidths=1, alpha=.7,
+            edgecolor='none',
+           s = 20,
+           c=(y_predSVR),
+            cmap=cm, vmin=-2, vmax=2)
+cbar = fig5.colorbar(sc)
+cbar.ax.set_ylabel('Change in bed level height per year [m] without xy', rotation=270)
+cbar.ax.get_yaxis().labelpad = 20
+ax[1].set_title('Prediction')
+ax[1].set_xlabel("x coordinate")
+ax[1].tick_params(labelsize=12)
+sc = ax[2].scatter(Xt['x'], Xt['y'],
+           linewidths=1, alpha=.7,
+            edgecolor='none',
+           s = 20,
+           c=(y_predSVR - yt),
+            cmap=cm,  vmin=-2, vmax=2)
+ax[2].set_title('Residual')
+ax[2].set_xlabel("x coordinate")
+ax[2].tick_params(labelsize=12)
+
+fig5.subplots_adjust(wspace=0.03, hspace=0)
+fig5.suptitle('SVR Annual sedimentation 2012-2018')
+plt.show()
+
+###################################
+# GPR PLOT
+###################################
+
+plt.rcParams.update({'font.size': 20})
+fig5, ax = plt.subplots(nrows=1, ncols=3, sharex=True, sharey=True,
+                                    figsize=(20, 10))
+cm = plt.cm.get_cmap('RdYlBu')
+sc = ax[0].scatter(Xt['x'], Xt['y'],
+           linewidths=1, alpha=.7,
+            edgecolor='none',
+           s = 20,
+           c=(yt),
+            cmap=cm, vmin=-2, vmax=2)
+ax[0].set_title('Actual')
+ax[0].set_xlabel("x coordinate")
+ax[0].set_ylabel("y coordinate")
+ax[0].tick_params(labelsize=12)
+sc = ax[1].scatter(Xt['x'], Xt['y'],
+           linewidths=1, alpha=.7,
+            edgecolor='none',
+           s = 20,
+           c=(y_predGPR),
+            cmap=cm, vmin=-2, vmax=2)
+cbar = fig5.colorbar(sc)
+cbar.ax.set_ylabel('Change in bed level height per year [m] without xy', rotation=270)
+cbar.ax.get_yaxis().labelpad = 20
+ax[1].set_title('Prediction')
+ax[1].set_xlabel("x coordinate")
+ax[1].tick_params(labelsize=12)
+sc = ax[2].scatter(Xt['x'], Xt['y'],
+           linewidths=1, alpha=.7,
+            edgecolor='none',
+           s = 20,
+           c=(y_predGPR - yt),
+            cmap=cm,  vmin=-2, vmax=2)
+ax[2].set_title('Residual')
+ax[2].set_xlabel("x coordinate")
+ax[2].tick_params(labelsize=12)
+
+fig5.subplots_adjust(wspace=0.03, hspace=0)
+fig5.suptitle('GPR Annual sedimentation 2012-2018')
 plt.show()
